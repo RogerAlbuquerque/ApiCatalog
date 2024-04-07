7578adf6f04c837f349b54bd4e27fb859631583f
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2024-04-07 08:47:55 -0300

    feat:Started to create first actions to handle database

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using ApiCatalog.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCatalog.Controllers;
+
+[Route("[controller]/{action}")]
+[ApiController]
+public class ProductController(AppDbContext context) : ControllerBase
+{
+    public readonly AppDbContext _context = context;
+
+}
