f52f632858c9852f2896bc0b13164652a103fb24
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2024-04-07 06:50:26 -0300

    feat:added datta annotations to class models

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections.ObjectModel;
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace ApiCatalog.Models;
 
+[Table("Categories")]
 public class Category
 {
     public Category()
     {
         Products = [];
     }
+
+    [Key]
     public int CategoryId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Name { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImageUrl { get; set; }
-
-    public ICollection<Product>? Products { get; set; }
-
+    public ICollection<Product> Products { get; set; }
 }
