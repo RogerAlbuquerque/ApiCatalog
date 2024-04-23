206b34ff979c538e3b36e80a9c9369a839d44a35
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2024-04-23 14:34:27 -0300

    feat:Implemented Concret class for category repository interface

---
+++ Repositores/CategoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using ApiCatalog.Models;
+
+namespace ApiCatalog.Repositores
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public Category CreateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category DeleteCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetCategoryById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
