d92ef3736ace6c70e19c30ab4fbdff31ee6daa43
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2024-04-23 14:29:54 -0300

    feat:Implemented Repository pattern - created interface for categories

---
+++ Repositores/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using ApiCatalog.Models;
+
+namespace ApiCatalog.Repositores
+{
+    public interface ICategoryRepository    // Since the interface is public, all its members are implicitly public as well.
+    {
+        IEnumerable<Category> GetCategories();        
+        Category GetCategoryById(int id);
+        Category CreateCategory(Category category);
+        Category UpdateCategory(Category category);
+        Category DeleteCategory(int id);
+    }
+
+}
