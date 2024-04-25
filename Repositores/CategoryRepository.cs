43044778d3252ed45302427a0866475c120994bf
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2024-04-25 11:09:36 -0300

    feat:Implemented scripts in concret class for interface of category

--- Repositores/CategoryRepository.cs
+++ Repositores/CategoryRepository.cs
@@ -1,32 +1,55 @@
-﻿using ApiCatalog.Models;
+﻿using ApiCatalog.Context;
+using ApiCatalog.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiCatalog.Repositores
 {
-    public class CategoryRepository : ICategoryRepository
+    public class CategoryRepository(AppDbContext context) : ICategoryRepository
     {
-        public Category CreateCategory(Category category)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Category DeleteCategory(int id)
-        {
-            throw new NotImplementedException();
-        }
+        private readonly AppDbContext _context = context;
 
         public IEnumerable<Category> GetCategories()
         {
-            throw new NotImplementedException();
+            return _context.Categories.ToList();
         }
 
         public Category GetCategoryById(int id)
         {
-            throw new NotImplementedException();
+            return _context.Categories.FirstOrDefault(c => c.CategoryId == id);
         }
+
+        public Category CreateCategory(Category category)
+        {
+            if (category is null) 
+                throw new ArgumentNullException(nameof(category));
+
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return category;
+        }
+
 
         public Category UpdateCategory(Category category)
         {
-            throw new NotImplementedException();
+            if (category is null)
+                throw new ArgumentNullException(nameof(category));
+
+            _context.Entry(category).State = EntityState.Modified;
+            _context.SaveChanges();
+            return category;
+
+        }
+
+        public Category DeleteCategory(int id)
+        {
+            var category = _context.Categories.Find(id);
+
+            if (category is null)
+                throw new ArgumentNullException(nameof(category));
+
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return category;
         }
     }
 }
