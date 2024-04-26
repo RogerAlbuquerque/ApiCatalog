36ae70a06c76dcc7aedf3932ae592567a76da9c0
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2024-04-26 07:01:22 -0300

    feat: implemented repository pattern to products

---
+++ Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using ApiCatalog.Models;
+
+namespace ApiCatalog.Repositories
+{   
+    // This is not the more usable way to implement repository, but, i need learn
+    public interface IProductRepository
+    {
+        IQueryable<Product> GetProducts();
+        Product GetProductById(int id);
+        Product CreatProduct(Product product);
+        bool UpdateProduct(Product product);
+        bool DeleteProduct(int id);
+    }
+}
---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,38 @@
+﻿using ApiCatalog.Models;
+
+namespace ApiCatalog.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Product GetProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Product> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Product CreatProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public bool UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public bool DeleteProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
