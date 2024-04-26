ad2a486554d39d9d64d66621974e01839b32284b
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2024-04-26 07:29:57 -0300

    feat: added half methods of productRepository

--- Repositories/ProductRepository.cs
+++ Repositories/ProductRepository.cs
@@ -1,38 +1,40 @@
-﻿using ApiCatalog.Models;
+﻿using ApiCatalog.Context;
+using ApiCatalog.Models;
 
-namespace ApiCatalog.Repositories
+namespace ApiCatalog.Repositories;
+
+public class ProductRepository(AppDbContext context) : IProductRepository
 {
-    public class ProductRepository : IProductRepository
+    private readonly AppDbContext _context = context;
+
+    public IQueryable<Product> GetProducts()
     {
-        public Product GetProductById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IQueryable<Product> GetProducts()
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public Product CreatProduct(Product product)
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public bool UpdateProduct(Product product)
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public bool DeleteProduct(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-
-
+        return _context.Products;
     }
+
+    public Product GetProductById(int id)
+    {
+        return _context.Products.FirstOrDefault(c => c.ProductId == id);
+    }
+
+
+    public Product CreatProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    public bool UpdateProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    public bool DeleteProduct(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+
+
 }
