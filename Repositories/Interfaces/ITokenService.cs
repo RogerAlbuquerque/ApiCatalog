27957e1b98db20d783da3f38d85c87c4ac08d4c2
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2024-06-12 14:33:18 -0300

    feat: added concret class to generate jwt token access

---
+++ Repositories/Interfaces/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace ApiCatalog.Repositories.Interfaces
+{
+    public interface ITokenService
+    {
+        JwtSecurityToken GenerateAccessToken(IEnumerable<Claim> claims, IConfiguration _config);    //claims is informations about user
+        string GenerateRefreshToken();
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token, IConfiguration _config);
+    }
+}
---
+++ Repositories/TokenService.cs
@@ -0,0 +1,42 @@
+﻿using ApiCatalog.Repositories.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ApiCatalog.Repositories
+{
+    public class TokenService : ITokenService
+    {
+        public JwtSecurityToken GenerateAccessToken(IEnumerable<Claim> claims, IConfiguration _config)
+        {
+            var key = _config.GetSection("JWT").GetValue<string>("SecretKey") ?? throw new InvalidOperationException("Invalid secret key");
+
+            var privateKey = Encoding.UTF8.GetBytes(key);
+
+            var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(privateKey), SecurityAlgorithms.HmacSha256Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(_config.GetSection("JWT").GetValue<double>("TokenValidityInMinutes")),
+                Audience = _config.GetSection("JWT").GetValue<string>("ValidityIssuer"),
+                SigningCredentials = signingCredentials
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateJwtSecurityToken(tokenDescriptor);
+
+            return token;
+        }
+
+        public string GenerateRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClaimsPrincipal GetPrincipalFromExpiredToken(string token, IConfiguration _config)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
