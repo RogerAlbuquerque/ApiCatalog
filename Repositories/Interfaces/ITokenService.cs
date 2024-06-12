6fb9da9d31953b8343523afe595a3264c614ffd6
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2024-06-12 16:18:35 -0300

    feat: added script to generate refresh token from expired token

--- Repositories/Interfaces/ITokenService.cs
+++ Repositories/Interfaces/ITokenService.cs
@@ -7,6 +7,6 @@ namespace ApiCatalog.Repositories.Interfaces
     {
         JwtSecurityToken GenerateAccessToken(IEnumerable<Claim> claims, IConfiguration _config);    //claims is informations about user
         string GenerateRefreshToken();
-        ClaimsPrincipal GetPrincipalFromExpiredToken(string token, IConfiguration _config);
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token, IConfiguration _config); //used to extract infos from claims of expired generated token and create new token
     }
 }
