63416559d439dc09b81f07f0396e2fd59931d377
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2024-06-13 12:50:13 -0300

    feat: AuthController added, and added 'login' action

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,72 @@
+﻿using ApiCatalog.DTOs;
+using ApiCatalog.Models;
+using ApiCatalog.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace ApiCatalog.Controllers;
+
+public class AuthController
+    (
+        ITokenService tokenService,
+        UserManager<ApplicationsUser> userManager,
+        RoleManager<IdentityRole> roleManager,
+        IConfiguration configuration
+    ) : ControllerBase
+{
+    private readonly ITokenService _tokenService = tokenService;
+    private readonly UserManager<ApplicationsUser> _userManager = userManager;
+    private readonly RoleManager<IdentityRole> _roleManager = roleManager;
+    private readonly IConfiguration _configuration = configuration;
+
+
+
+
+
+    [HttpPost]
+    [Route("login")]
+    public async Task<IActionResult> Login([FromBody] LoginModel model)
+    {
+        var user = await _userManager.FindByNameAsync(model.Username);
+        if (user is not null && await _userManager.CheckPasswordAsync(user, model.Password))
+        {
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            foreach (var userRole in userRoles)
+            {
+                authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+            }
+
+            var token = _tokenService.GenerateAccessToken(authClaims, _configuration);
+
+            var refreshToken = _tokenService.GenerateRefreshToken();
+
+            _ = int.TryParse(_configuration["JWT:RefreshTokenValidityInMinutes"], out int refreshTokenValidityInMinutes);
+
+            user.RefreshToken = refreshToken;
+
+            user.RefreshTokenExpiryTime = DateTime.Now.AddMinutes(refreshTokenValidityInMinutes);
+
+            await _userManager.UpdateAsync(user);
+
+            return Ok(
+                new
+                {
+                    Token = new JwtSecurityTokenHandler().WriteToken(token),
+                    RefreshToken = refreshToken,
+                    Expiration = token.ValidTo
+                });
+        }
+
+        return Unauthorized();
+    }
+}
